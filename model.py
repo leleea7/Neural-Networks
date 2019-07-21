ba7276b1c377df0d0961ee67e3588713b8847dbb
Emanuele Alessi <34547327+leleea7@users.noreply.github.com>
2019-07-21 14:58:53 +0200

    Update model.py

--- model.py
+++ model.py
@@ -156,7 +156,7 @@ class MultiTaskRecognizer:
                         l = np.array(landmarks[landmarks['image_id'] == row])[0][1:]
                         x = np.array([l[i] for i in range(0, len(l), 2)]) * self.__shape[0] / size[0]
                         y = np.array([l[i] for i in range(1, len(l), 2)]) * self.__shape[1] / size[1]
-                        img = np.expand_dims(img, axis=-1)
+                        #img = np.expand_dims(img, axis=-1)
                         land = np.concatenate([x, y])
                         pred = self.__session.run([self.__out[task] for task in self.__tasks],
                                                   feed_dict={self.__images: [img]},
